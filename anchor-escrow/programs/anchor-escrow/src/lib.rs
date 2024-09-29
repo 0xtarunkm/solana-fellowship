24c98af38d9f9bce6377200d2893e6e2beb6c729
tarunclub <tarunkm__@outlook.com>
2024-09-29 17:54:13 +0530

    feat: make instruction

---
+++ anchor-escrow/programs/anchor-escrow/src/lib.rs
@@ -0,0 +1,18 @@
+use anchor_lang::prelude::*;
+
+pub mod instructions;
+pub mod states;
+
+pub use instructions::*;
+
+declare_id!("CiU2bNrk2dnxSgUe6wJCY6xwNaS77ZQDvnzAb2ietGBF");
+
+#[program]
+pub mod anchor_escrow {
+    use super::*;
+
+    pub fn make(ctx: Context<Make>, seed: u64, receive: u64, amount: u64) -> Result<()> {
+        ctx.accounts.save_escrow(seed, receive, &ctx.bumps)?;
+        ctx.accounts.deposit_to_vault(amount)
+    }
+}
