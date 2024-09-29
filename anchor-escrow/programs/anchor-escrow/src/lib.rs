ce1c44b328044493251bde70987494f8d007bf32
tarunclub <tarunkm__@outlook.com>
2024-09-29 18:13:58 +0530

    feat: take and refund

--- anchor-escrow/programs/anchor-escrow/src/lib.rs
+++ anchor-escrow/programs/anchor-escrow/src/lib.rs
@@ -15,4 +15,13 @@ pub mod anchor_escrow {
         ctx.accounts.save_escrow(seed, receive, &ctx.bumps)?;
         ctx.accounts.deposit_to_vault(amount)
     }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.transfer_to_maker()?;
+        ctx.accounts.withdraw_and_close()
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.withdraw_and_close()
+    }
 }
